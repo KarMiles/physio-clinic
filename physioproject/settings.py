467e9a33db5a6eda9d7beb4b9dab8d213138f6aa
Karol Mileszko <karol.miles@gmail.com>
2022-06-21 13:56:09 +0000

    Reintroduce physioproject

--- physioproject/settings.py
+++ physioproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^c@+5--0ik(g9bb5!50p-u@!3j1f_3(f8*!p@khjzhz0bbqe(l'
+SECRET_KEY = 'django-insecure-bfy7#qgc8&=#%ip%qd$jsh)-6jgztk0q^*5@%==u*@h&2!p(61'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
