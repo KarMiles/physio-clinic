704e16a50cf48ec634557b43e2389e8c1342dfa1
Karol Mileszko <karol.miles@gmail.com>
2022-06-25 09:36:53 +0000

    Create blank post_detail.html

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Post
+from .models import Post, Comment
 
 
 # Models
@@ -14,6 +14,16 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content')
     list_filter = ('status', 'priority', 'updated_on', 'author')
 
-
 # Replaced by class decoration:
 # admin.site.register(Post)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('author', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
