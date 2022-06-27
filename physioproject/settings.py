38336e5002877d4fc3a6861a88ac2b7511d634c9
Karol Mileszko <karol.miles@gmail.com>
2022-06-27 10:46:10 +0000

    Add details in post_detail.html

--- physioproject/settings.py
+++ physioproject/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'blog',
     'booking',
 ]
@@ -61,6 +62,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
