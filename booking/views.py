fd8ac3ead533c8266ba25a25beacfec91738e7b8
Karol Mileszko <karol.miles@gmail.com>
2022-07-07 16:48:43 +0000

    Clean up bookings/views.py

--- booking/views.py
+++ booking/views.py
@@ -11,25 +11,18 @@ from .forms import BookingForm
 
 class BookingList(LoginRequiredMixin, generic.ListView):
     model = Booking
-    # queryset = Booking.objects.filter(status=0).order_by("updated_on")
     template_name = "booking.html"
     context_object_name = 'bookings'
 
     def get_queryset(self):
         return Booking.objects.filter(status=0, user=self.request.user).order_by("updated_on")
 
-
-# class BookingDetail(View):
-
     def get(self, *args, **kwargs):
-        # queryset = Booking.objects.filter(status=0)
-        # bookings = get_object_or_404(queryset, pk=booking_id)
 
         return render(
             self.request,
             "booking.html",
             {
-                # "bookings": bookings,
                 "bookings": self.get_queryset(),
                 "booking_form": BookingForm()
             },
@@ -37,24 +30,18 @@ class BookingList(LoginRequiredMixin, generic.ListView):
 
     def post(self, *args, **kwargs):
 
-        # queryset = Booking.objects.filter(status=0)
-        # booking = get_object_or_404(queryset, pk=Booking.user)
-
         booking_form = BookingForm(data=self.request.POST)
         if booking_form.is_valid():
             booking_form.instance.user = self.request.user
             booking = booking_form.save()
             return redirect(reverse('booking'))
+
         else:
-            # booking_form = BookingForm()
-        
             return render(
                 self.request,
                 "booking.html",
                 {
-                    # "bookings": bookings,
                     "bookings": self.get_queryset(),
-                    # "booking_form": BookingForm()
                     "booking_form": booking_form
                 }
             )
