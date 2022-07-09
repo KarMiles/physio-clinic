673ea42544dd9f90db7439b553ca52b27cb4d850
Karol Mileszko <karol.miles@gmail.com>
2022-07-09 14:31:18 +0000

    Add excerpt to post detail page

--- blog/views.py
+++ blog/views.py
@@ -61,7 +61,7 @@ class PostDetail(View):
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
-        
+
         return render(
             request,
             "post_detail.html",
