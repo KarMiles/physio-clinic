b2a00861fb8825dbbcbdb503c4e29386a4e9910b
Karol Mileszko <karol.miles@gmail.com>
2022-07-12 19:27:03 +0000

    Add confirmation message

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy, reverse
 from django.utils.text import slugify
 from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
-# from django.contrib import messages
+from django.contrib import messages
 
 from .models import Post
 from .forms import CommentForm, PostForm
@@ -32,7 +32,9 @@ class CreatePost(generic.CreateView):
         self.object = form.instance
         self.object.author = self.request.user
         self.object.slug = slugify(self.object.title)
-        return super().form_valid(form)
+        response = super().form_valid(form)
+        messages.add_message(self.request, messages.INFO, 'Hello world.')
+        return response
 
     def get_success_url(self):
         return reverse('post_detail', args=[self.object.slug])
@@ -85,16 +87,24 @@ class DeletePost(LoginRequiredMixin, StaffRequiredMixin, generic.DeleteView):
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by("priority")
+    # queryset = Post.objects.filter(status=1).order_by("priority")
     template_name = "index.html"
     paginate_by = 3
+    
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Post.objects.order_by("priority")
+        else:
+            return Post.objects.filter(status=1).order_by("priority")
+
+        
 
 
 class PostDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
 
-        queryset = Post.objects.filter(status=1)
+        queryset = self.get_queryset()
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("created_on")
         liked = False
@@ -115,7 +125,8 @@ class PostDetail(View):
 
     def post(self, request, slug, *args, **kwargs):
 
-        queryset = Post.objects.filter(status=1)
+        queryset = self.get_queryset()
+        # queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
@@ -144,6 +155,12 @@ class PostDetail(View):
                 "success": 'Comment sent for approval'
             },
         )
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Post.objects.order_by("priority")
+        else:
+            return Post.objects.filter(status=1).order_by("priority")
 
 
 class PostLike(View):
