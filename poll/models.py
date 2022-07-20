0e816a20fad58fe480fa846a71aa955c4d8418a2
Karol Mileszko <karol.miles@gmail.com>
2022-07-20 12:47:25 +0000

    Make Poll model

--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Models for poll app
+
+class Poll(models.Model):
+    question = models.TextField()
+    option_one = models.CharField(max_length=50)
+    option_two = models.CharField(max_length=50)
+    option_three = models.CharField(max_length=50)
+    option_one_count = models.IntegerField(default=0)
+    option_two_count = models.IntegerField(default=0)
+    option_three_count = models.IntegerField(default=0)
