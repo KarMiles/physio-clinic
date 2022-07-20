1cec165ca80a253ade9d3b35a2b552b8f3fc9f1e
Karol Mileszko <karol.miles@gmail.com>
2022-07-20 16:23:20 +0000

    Prepare front end poll_base.html functionality

--- poll/urls.py
+++ poll/urls.py
@@ -7,8 +7,8 @@ from . import views as poll_views
 # URL patterns for the app poll
 
 urlpatterns = [
-    path('', poll_views.home, name='poll_home'),
-    path('create/', poll_views.create, name='poll_create'),
-    path('results/', poll_views.results, name='poll_results'),
-    path('vote/', poll_views.vote, name='poll_vote'),
+    path('', poll_views.poll_home, name='poll_home'),
+    path('create/', poll_views.poll_create, name='poll_create'),
+    path('vote/<poll_id>/', poll_views.poll_vote, name='poll_vote'),
+    path('results/<poll_id>', poll_views.poll_results, name='poll_results'),
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
+from .forms import CreatePollForm
 
 # Poll views
 
-def home(request):
+def poll_home(request):
     context = {}
     return render(request, 'poll/poll_home.html', context)
 
-def create(request):
-    context = {}
+def poll_create(request):
+    if request.method == 'POST':
+        form = CreatePollForm(request.POST)
+    else:
+        form = CreatePollForm()
+    context = {'form': form}
     return render(request, 'poll/poll_create.html', context)
 
-def results(request):
+def poll_results(request):
     context = {}
     return render(request, 'poll/poll_results.html', context)
 
-def vote(request):
+def poll_vote(request):
     context = {}
     return render(request, 'poll/poll_vote.html', context)
