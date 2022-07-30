ee9dd90ee220d513061115753109569d59f99f2c
Karol Mileszko <karol.miles@gmail.com>
2022-07-30 11:25:44 +0000

    Add 404 and 500 hanlders in physioproject/urls.py

--- physioproject/urls.py
+++ physioproject/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-# from django.views.generic import TemplateView
+
+# Internal:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 # URL patterns for the project physioproject
@@ -32,3 +37,6 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
     path('poll/', include('poll.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'blog.views.error_404_view'
+handler500 = 'blog.views.error_500_view'
