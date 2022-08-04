e67ae522125a7f4d0eba14fed79c6eb95691933a
Karol Mileszko <karol.miles@gmail.com>
2022-08-04 09:21:15 +0000

    Change STATICFILES_DIR for Heroku

--- physioproject/settings.py
+++ physioproject/settings.py
@@ -185,11 +185,11 @@ STATIC_URL = '/static/'
 
 # Code to tell Django to use Cloudinary to store media and static files
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Version of above from Heroku Devcenter:
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
