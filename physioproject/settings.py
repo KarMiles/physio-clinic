029e2b62995933c18337ce651fe515ad86066f91
Karol Mileszko <karol.miles@gmail.com>
2022-08-05 16:35:12 +0000

    Add if statement in debug settings

--- physioproject/settings.py
+++ physioproject/settings.py
@@ -41,7 +41,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+
+DEBUG = os.environ.get('DEBUG', '1') == '1'
 
 ALLOWED_HOSTS = ["physio-clinic-heroku.herokuapp.com", "localhost"]
 
