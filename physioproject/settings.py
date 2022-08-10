0710d7e263611009f8e22978250e20f6b066765e
Karol Mileszko <karol.miles@gmail.com>
2022-08-10 12:48:56 +0000

    Change email settings

--- physioproject/settings.py
+++ physioproject/settings.py
@@ -165,14 +165,15 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    else: EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' EMAIL_USE_TLS = True EMAIL_PORT = 587 EMAIL_HOST = 'smtp.gmail.com' EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS') DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-    EMAIL_USE_SSL = strtobool(os.getenv("EMAIL_USE_SSL", "false"))
-    EMAIL_HOST = os.environ.get('EMAIL_HOST')
-    EMAIL_USE_TLS = strtobool(os.getenv("EMAIL_USE_TLS", "false"))
-    EMAIL_PORT = os.environ.get('EMAIL_PORT')
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    # EMAIL_USE_SSL = strtobool(os.getenv("EMAIL_USE_SSL", "false"))
+    # EMAIL_HOST = os.environ.get('EMAIL_HOST')
+    # EMAIL_USE_TLS = strtobool(os.getenv("EMAIL_USE_TLS", "false"))
+    # EMAIL_PORT = os.environ.get('EMAIL_PORT')
+    # EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
     DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
     # ADMINS = [('Karol', 'karmiles.dev@gmail.com')]
