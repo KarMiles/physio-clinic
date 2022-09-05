ff17bc12ce2bdf9d7b7a97f735b6c7dbeafa81bb
Karol Mileszko <karol.miles@gmail.com>
2022-09-05 13:05:49 +0100

    Add muted text to high contrast scheme

--- static/js/script.js
+++ static/js/script.js
@@ -11,9 +11,11 @@ setTimeout(function () {
 
 var highContrastBlue = '#164aa6';
 var highContrastRed = '#ab2237';
+var highContrastMute = '#000000';
 
 var logoblue = ' #3186e0';
 var logored = ' #eb7575';
+var mute = ' darkgrey';
 
 // Get the root element
 var r = document.querySelector(':root');
@@ -22,31 +24,33 @@ var r = document.querySelector(':root');
 function toggleContrast() {
     var rs = getComputedStyle(r);
     var current_blue = rs.getPropertyValue('--logoblue');
-    // var current_red = rs.getPropertyValue('--logored');
 
     if (current_blue == logoblue) {
         r.style.setProperty('--logoblue', highContrastBlue);
         r.style.setProperty('--logored', highContrastRed);
+        r.style.setProperty('--mute', highContrastMute);
 
         localStorage.setItem('current_blue', highContrastBlue);
         localStorage.setItem('current_red', highContrastRed);
-
-        // localStorage.setItem('current_blue', highContrastBlue);
-        // localStorage.setItem('current_red', highContrastRed);
+        localStorage.setItem('current_mute', highContrastMute);
     }
     else {
         r.style.setProperty('--logoblue', logoblue);
         r.style.setProperty('--logored', logored);
+        r.style.setProperty('--mute', mute);
 
         localStorage.setItem('current_blue', logoblue);
         localStorage.setItem('current_red', logored);
+        localStorage.setItem('current_mute', mute);
     };
 }
 
 function contrastFunction_maintain() {
     let current_blue = localStorage.getItem('current_blue');
     let current_red = localStorage.getItem('current_red');
+    let current_mute = localStorage.getItem('current_mute');
 
     r.style.setProperty('--logoblue', current_blue);
     r.style.setProperty('--logored', current_red);
+    r.style.setProperty('--mute', current_mute);
 }
